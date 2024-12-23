c31038e59e9d5728b47e44659e6061f95d965288
John <johntheherrera@gmail.com>
2024-12-22 23:47:10 -0800

    lets not break it now

--- js/wordsearchcontroller.js
+++ js/wordsearchcontroller.js
@@ -19,40 +19,7 @@ function WordSearchController(gameId, listId, solveId, newGameId, instructionsId
 	var searchTypes = {
 
 		"Math! (please don't run away)": [["asymptote", "differential", "algorithm", "boolean"],
-			["euclidean", "integral", "logarithm", "matrix"],
-			["riemann", "polyhedron", "theta", "vector"],
-			["binomial", "pythagoras", "eccentricity", "unit circle"],
-			["derivative",  "polar coordinates",  "tangent", "scalene"]],
-
-		"Astronomy and Physics!": [["circumpolar", "comet", "asteroid", "declination"],
-			["earthshine", "albedo", "quantum", "olivine"], 
-			["pyroxene", "decoherence", "fermion", "quark"],
-			["gluon", "redshift", "inflaton", "planetesimal"],
-			["anthropic", "exogenesis", "atom", "planck"]],
-
-		"Philosophy!": [["metaphysics", "modus ponens", "modus tollens", "analogy"],
-			["a priori", "a posteriori", "conditional", "nietzsche"],
-			["diogenes", "paradox", "occam's razor", "causality"],
-			["induction", "deduction", "ontology", "theology"],
-			["syllogism", "ethics", "karl marx", "pluralism"]],
-
-		"World Mythology :D": [["chronos", "aether", "hypnos", "psyche"],
-			["jupiter", "sol", "chaos", "pandora"],
-			["thor", "valhalla", "amaterasu", "osiris"],
-			["mazu", "izanami", "susanoo", "xipe totec"],
-			["mercury", "bastet", "sekhmet", "ptah"]],
-
-		"Shades of Purple!": [["violet", "periwinkle", "plum", "grape"],
-			["orchid", "wine", "mauve", "lavender"],
-			["lilac", "mulberry", "eggplant", "heliotrope"],
-			["liseran purple", "amethyst", "fuchsia", "pomp and power"],
-			["sangria", "boysenberry", "thistle", "heather"]],
-
-		"The Many Different Flavors of Cat!": [["Russian Blue", "Siamese", "Persian", "Sphynx"],
-			["Ragdoll", "Singapura", "Snowshoe", "Turkish Van"],
-			["Maine Coon", "Devon Rex", "Charteux", "Scottish Fold"],
-			["Himalayan", "Ragamuffin", "Bombay", "Siberian"],
-			["Egyptian Mau", "Norwegian Forest Cat", "Abyssinian", "York Chocolate"]]
+			["euclidean", "integral", "logarithm", "matrix"]]
 
 	};
 
